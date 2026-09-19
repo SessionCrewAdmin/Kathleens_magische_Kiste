5950cd2f2601658c8af45d3ab982a4b3868bbe3d
SessionCrewAdmin <jonach54@googlemail.com>
2026-09-19 20:46:28 +0200

    V22.3: refresh Board UX and Text Engine assets

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='kathleen-board-v22-1-2-hotfix-20260919';
+const CACHE='kathleen-board-v22-3-text-engine-20260919';
 const CORE=[
   './','./index.html','./manifest.webmanifest',
   './tools/english-world-quiz/index.html','./tools/english-world-quiz/bonus.html','./tools/homework-vouchers/index.html',
