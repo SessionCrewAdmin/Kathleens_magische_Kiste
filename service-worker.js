49f57f7d7fcca65b06a6e808a84c157f59601bae
SessionCrewAdmin <jonach54@googlemail.com>
2026-09-19 20:56:05 +0200

    V22.4: refresh Dynamic Tool Island assets

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='kathleen-board-v22-3-2-bottom-structural-20260919';
+const CACHE='kathleen-board-v22-4-dynamic-island-20260919';
 const CORE=[
   './','./index.html','./manifest.webmanifest',
   './tools/english-world-quiz/index.html','./tools/english-world-quiz/bonus.html','./tools/homework-vouchers/index.html',
