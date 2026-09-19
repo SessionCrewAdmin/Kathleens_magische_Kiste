45377b5dc2c04ec6fbae9e6b80d3aa120608162e
SessionCrewAdmin <jonach54@googlemail.com>
2026-09-19 19:49:25 +0200

    V20.2: refresh student eraser UI

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='kathleen-board-v20-1-max-20260919';
+const CACHE='kathleen-board-v20-2-max-20260919';
 const CORE=[
   './','./index.html','./manifest.webmanifest',
   './tools/english-world-quiz/index.html','./tools/english-world-quiz/bonus.html','./tools/homework-vouchers/index.html',
