f3ed4b8cde4b4cde1ffe20aba646d75632284ee6
SessionCrewAdmin <jonach54@googlemail.com>
2026-09-19 18:45:56 +0200

    V18.1: refresh Control Room window assets

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='kathleen-board-v18-20260919';
+const CACHE='kathleen-board-v18-1-20260919';
 const CORE=[
   './','./index.html','./manifest.webmanifest',
   './tools/english-world-quiz/index.html','./tools/english-world-quiz/bonus.html','./tools/homework-vouchers/index.html',
