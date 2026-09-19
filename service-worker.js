e2353045429be2df35e58ddad6e367c87db4b6ad
SessionCrewAdmin <jonach54@googlemail.com>
2026-09-19 21:21:20 +0200

    V23: refresh complete Classroom Board 2.0 assets

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='kathleen-board-v22-4-4-glass-island-20260919';
+const CACHE='kathleen-board-v23-full-widgets-20260919';
 const CORE=[
   './','./index.html','./manifest.webmanifest',
   './tools/english-world-quiz/index.html','./tools/english-world-quiz/bonus.html','./tools/homework-vouchers/index.html',
