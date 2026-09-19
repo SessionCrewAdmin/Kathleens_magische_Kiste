89bafa2f663debd6e56060ce35824451ce2579c4
SessionCrewAdmin <jonach54@googlemail.com>
2026-09-19 20:33:34 +0200

    V22.1.1: refresh broken Classroom Board cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='kathleen-board-v22-1-timer-traffic-20260919';
+const CACHE='kathleen-board-v22-1-1-hotfix-20260919';
 const CORE=[
   './','./index.html','./manifest.webmanifest',
   './tools/english-world-quiz/index.html','./tools/english-world-quiz/bonus.html','./tools/homework-vouchers/index.html',
