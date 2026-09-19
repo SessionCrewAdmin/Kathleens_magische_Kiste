18a45b723dd2793963fd6fa0fbc52f6f00e2aeca
SessionCrewAdmin <jonach54@googlemail.com>
2026-09-19 21:53:39 +0200

    V23.1: final cache rotation after Kiste cleanup

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='kathleen-v23-1-unified-poll-kiste-cleanup-20260919';
+const CACHE='kathleen-v23-1-unified-poll-kiste-cleanup-final-20260919';
 const CORE=[
   './','./index.html','./manifest.webmanifest',
   './tools/english-world-quiz/index.html','./tools/english-world-quiz/bonus.html','./tools/homework-vouchers/index.html',
