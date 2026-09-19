6672a6b70d9f5c612e07045b10f44622e9a43c6e
SessionCrewAdmin <jonach54@googlemail.com>
2026-09-19 19:10:07 +0200

    V20: refresh max student workspace assets

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='kathleen-board-v19-1-20260919';
+const CACHE='kathleen-board-v20-max-20260919';
 const CORE=[
   './','./index.html','./manifest.webmanifest',
   './tools/english-world-quiz/index.html','./tools/english-world-quiz/bonus.html','./tools/homework-vouchers/index.html',
