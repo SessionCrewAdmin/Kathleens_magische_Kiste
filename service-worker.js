e3b3f73e29d697bfe97803130e5863e9fde0ce84
SessionCrewAdmin <jonach54@googlemail.com>
2026-09-19 21:39:32 +0200

    Live Poll V21.5: refresh multi-type poll assets

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='kathleen-live-poll-v21-4-glass-20260919';
+const CACHE='kathleen-live-poll-v21-5-types-charts-20260919';
 const CORE=[
   './','./index.html','./manifest.webmanifest',
   './tools/english-world-quiz/index.html','./tools/english-world-quiz/bonus.html','./tools/homework-vouchers/index.html',
