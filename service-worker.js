b74ab40269bb4517974abbb759222a755a6a01f0
SessionCrewAdmin <jonach54@googlemail.com>
2026-09-21 01:48:56 +0200

    Refresh cache for seating plan tags

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='kathleen-v24-seating-ui-fix-20260921';
+const CACHE='kathleen-v24-seating-tags-20260921';
 const CORE=[
   './','./index.html','./manifest.webmanifest',
   './tools/english-world-quiz/index.html','./tools/english-world-quiz/bonus.html','./tools/homework-vouchers/index.html',
