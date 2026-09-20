1406f6d7027693c7652fdea7e01b951efac04bc0
SessionCrewAdmin <jonach54@googlemail.com>
2026-09-20 10:54:33 +0200

    V23.4: final cache rotation for Push Center and Frau Müller UI

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='kathleen-v23-4-teacher-push-20260920';
+const CACHE='kathleen-v23-4-teacher-push-final-20260920';
 const CORE=[
   './','./index.html','./manifest.webmanifest',
   './tools/english-world-quiz/index.html','./tools/english-world-quiz/bonus.html','./tools/homework-vouchers/index.html',
