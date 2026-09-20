727849e01a2bd12058d67003b9810ff420a4317e
SessionCrewAdmin <jonach54@googlemail.com>
2026-09-20 15:47:19 +0200

    V23.5: rotate cache for notenzentrale update

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='kathleen-v23-4-exact-boat-reveal-20260920';
+const CACHE='kathleen-v23-5-notenzentrale-20260920';
 const CORE=[
   './','./index.html','./manifest.webmanifest',
   './tools/english-world-quiz/index.html','./tools/english-world-quiz/bonus.html','./tools/homework-vouchers/index.html',
