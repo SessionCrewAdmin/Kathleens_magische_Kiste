0a65cf405fcd40b5dd93f812fed8559432d049a4
SessionCrewAdmin <jonach54@googlemail.com>
2026-09-22 16:07:35 +0200

    PWA: refresh mobile observation notes

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='kathleen-v37-2-mobile-observations-20260922';
+const CACHE='kathleen-v37-2-mobile-notes-20260922';
 const CORE=['./tools/mobile-observations/index.html','./tools/seating-plan/mobile-view.js','./mobile-teacher-v34.js',
   './','./index.html','./home-todo-v2.js','./manifest.webmanifest',
   './tools/english-world-quiz/index.html','./tools/english-world-quiz/bonus.html','./tools/homework-vouchers/index.html',
