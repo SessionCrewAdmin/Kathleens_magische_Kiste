7c667bf060dd8cc46b6e53f6c36c58891d8f0947
SessionCrewAdmin <jonach54@googlemail.com>
2026-09-22 07:25:06 +0200

    PWA V36.1: activate iPhone navigation fixes

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='kathleen-v36-mobile-session-20260922';
+const CACHE='kathleen-v36-1-mobile-nav-fix-20260922';
 const CORE=['./mobile-teacher-v34.js',
   './','./index.html','./home-todo-v2.js','./manifest.webmanifest',
   './tools/english-world-quiz/index.html','./tools/english-world-quiz/bonus.html','./tools/homework-vouchers/index.html',
